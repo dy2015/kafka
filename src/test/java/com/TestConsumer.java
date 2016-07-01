840625e8a21871363371a60a4a0b47a10723e4ae
dy <2711099209@qq.com>
2016-07-01 19:00:32 +0800

    修改

--- src/test/java/com/TestConsumer.java
+++ src/test/java/com/TestConsumer.java
@@ -6,7 +6,7 @@ public class TestConsumer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new kafkaConsumer("dy").start();// 使用kafka集群中创建好的主题 test
+		new kafkaConsumer("test").start();// 使用kafka集群中创建好的主题 test
 	}
 
 }
